c5a0727dfb840eab178ae777b87ccce0b2f2c393
Aky <Aky@aky.aky>
2021-02-25 23:57:57 +0900

    2021-02-25 tuneup-periodic-08

--- chart/wsgi.py
+++ chart/wsgi.py
@@ -60,12 +60,10 @@ def update_source(source):
                     while not done:
                         try:
                             csv_str(source, csv_data)
-                            time.sleep(2)
                             updated(source.pk, last_modified)
-                            time.sleep(1)
                             up_image(source)
                             done = True
-                        except Exception: time.sleep(7)
+                        except Exception: time.sleep(1)
                     print('End Update', source)
                 else:
                     wait_time = 3600  # 更新がないとき 1時間後に再実行
@@ -88,10 +86,9 @@ def update_process(process):
             while not done:
                 try:
                     updated_p(process.pk, max(source1_up, source2_up))
-                    time.sleep(1)
                     up_image_p(process)
                     done = True
-                except Exception: time.sleep(7)
+                except Exception: time.sleep(1)
             print('End Update-P', process)
         else:
             wait_time = 3600  # 更新がないとき 1時間後に再実行
@@ -104,13 +101,13 @@ def checkup():
         t_update = threading.Thread(
             target=update_source, kwargs={'source': source})
         t_update.start()
-        time.sleep(5)
+        time.sleep(1)
 
     for process in Process.objects.all():
         t_update_p = threading.Thread(
             target=update_process, kwargs={'process': process})
         t_update_p.start()
-        time.sleep(5)
+        time.sleep(1)
 
 
 t_awake = threading.Thread(target=awake)
